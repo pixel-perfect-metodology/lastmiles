c2ca1ae1f8d986b5074ed7ec01f483f13b287980
blastwave <dclarke@blastwave.org>
2020-05-06 22:28:33 +0000

    20200506222833 : simple code lesson stuff for Jess

---
+++ integer/hfj.c
@@ -0,0 +1,30 @@
+
+/*********************************************************************
+ * The Open Group Base Specifications Issue 6
+ * IEEE Std 1003.1, 2004 Edition
+ *
+ *    An XSI-conforming application should ensure that the feature
+ *    test macro _XOPEN_SOURCE is defined with the value 600 before
+ *    inclusion of any header. This is needed to enable the
+ *    functionality described in The _POSIX_C_SOURCE Feature Test
+ *    Macro and in addition to enable the XSI extension.
+ *
+ *********************************************************************/
+#define _XOPEN_SOURCE 600
+
+#include <stdio.h>
+
+int main() {
+
+    int number = 12345 ;
+
+    printf ( "hello there you people " );
+
+    printf ( "%d", number );
+
+    printf ( "\n" );
+
+    return 42 ;
+
+}
+
---
+++ integer/hubble_for_jess.c
@@ -0,0 +1,25 @@
+
+
+
+
+
+#include <stdio.h>
+
+
+
+        int          main()
+
+
+              {
+
+           int number = 12345    ;
+
+     printf   ( "hello there you people   "   )   ;
+
+
+                return 42 ;
+
+
+               }
+
+
